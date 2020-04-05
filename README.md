4aba33de7ec2e4975e6f99eab04fab5eecab7306
James McBean <james.mcbean@rogers.com>
2020-04-05 11:19:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WORD COUNT
+
+Based on the Django wordcount project by Nick Walter.
+
+A simple Djando application that will count the words in an HTML textarea.
+Each word is lower-cased and special characters are removed from the beginning and end of the word.  
---
+++ wordcount/wordcount/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    full_text = request.GET['fulltext']
+
+    word_list = full_text.split()
+    word_dictionary = {}
+    for word in word_list:
+        lower_cased = word.lower().strip(",-.;:?!")
+        if len(lower_cased) == 0:
+            continue
+        if lower_cased in word_dictionary:
+            word_dictionary[lower_cased] += 1
+        else:
+            word_dictionary[lower_cased] = 1
+
+    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': full_text, 'count': len(word_list), 
+                           'sortedwords': sorted_words})
+
+def about(request):
+    return render(request, 'about.html')
+    
