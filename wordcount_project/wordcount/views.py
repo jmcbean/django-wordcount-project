af14f3b854d3250b6cc1ed0f939b96ce3970cbe8
James McBean <james.mcbean@rogers.com>
2020-04-12 12:25:58 -0400

    correct word count; improve web page

--- wordcount_project/wordcount/views.py
+++ wordcount_project/wordcount/views.py
@@ -10,19 +10,22 @@ def count(request):
 
     word_list = full_text.split()
     word_dictionary = {}
+    word_count = 0
     for word in word_list:
         lower_cased = word.lower().strip(",-.;:?!")
         if len(lower_cased) == 0:
             continue
+        word_count += 1
         if lower_cased in word_dictionary:
             word_dictionary[lower_cased] += 1
         else:
             word_dictionary[lower_cased] = 1
 
-    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_by_word = sorted(word_dictionary.items(), key=operator.itemgetter(0))
+    sorted_words_by_count = sorted(sorted_by_word, key=operator.itemgetter(1), reverse=True)
 
-    return render(request, 'count.html', {'fulltext': full_text, 'count': len(word_list), 
-                           'sortedwords': sorted_words})
+    return render(request, 'count.html', {'fulltext': full_text, 'count': word_count, 
+                           'sortedwords': sorted_words_by_count})
 
 def about(request):
     return render(request, 'about.html')
